4d90774b88e463bbb83f8463323b29313d5b9672
janainamaria <janainamariamdm@gmail.com>
2022-06-28 08:15:35 -0300

    chamada para api

--- src/components/MenuModal/MenuModal.js
+++ src/components/MenuModal/MenuModal.js
@@ -1,16 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import ReactCircleModal from 'react-circle-modal'
 import { AiOutlineClose } from "react-icons/ai";
 import './MenuModal.css'
 import api from '../../utils/api/api'
 import axios from "axios";
 
-let cep;
-function handleChange(e) {
-  cep+=e.target.value
-}
- function cepVerify(e){
-  console.log(cep)
+let state = {value: ''};
+
+
+//  function cepVerify(e){
+
 //    let cepNumber = document.getElementById('cep').value
 //   console.log(e.target)
 //   let cepCall;
@@ -33,10 +32,33 @@ function handleChange(e) {
 //   }else{
 //     console.log("Nenhum CEP enviado")
 //   }
-  e.preventDefault();
-}
+//   e.preventDefault();
+// }
 
 const Menu = () => {
+
+
+   function buscaCep(e) {
+    let cepCall;
+    if(e.keyCode === 13){
+  let options = {
+    method:'GET',
+    mode:'cors',
+    cache:'default'
+  }
+
+    let CEP = e.target.value
+     fetch(`https://viacep.com.br/ws/${CEP}/json/`,options)
+    .then(response => response.json())
+    .then((data) => {
+      if(data.cep){
+        alert("CEP encontrado! seu material de apoio esta a caminho!")
+      }else{
+        alert("CEP não encontrado!")
+      }
+    })
+  }
+  }
     return (
       <ReactCircleModal
         backgroundColor="var(--grey)"
@@ -57,12 +79,9 @@ const Menu = () => {
             </a>
             <div class="login-page">
             <div class="form">
-              <form class="login-form" onSubmit={cepVerify}>
-                <input type="text" placeholder="Digite seu cep" onChange={handleChange}/>
-            
-                <button className='search' type='submit' >Confirmar</button>
-                <p id="result"></p>
-           
+              <h3 className="titleModal">Consultar CEP</h3>
+              <form class="login-form" onSubmit={(e)=>{e.preventDefault()}}>
+                <input type="text"  name="CEP" placeholder="Digite seu cep" onKeyDown={(e)=>{buscaCep(e)}}/>
               </form>
             </div>
           </div></Fragment>
